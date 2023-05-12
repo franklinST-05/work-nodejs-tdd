23c5eb24f24195190739e2d6f3089c0c317fdd98
Franklin Silva <franklinteixeira205@gmail.com>
2023-05-12 08:28:46 -0300

    refactor: implements interface controller

--- src/apresentation/controllers/signup.ts
+++ src/apresentation/controllers/signup.ts
@@ -1,6 +1,7 @@
+import { Controller } from '../protocols/controller';
 import { HttpRequest, HttpResponse } from '../protocols/http';
 
-export class SignUpController {
+export class SignUpController implements Controller {
     handle(httpRequest: HttpRequest): HttpResponse {
         return {
             statusCode: 400,
