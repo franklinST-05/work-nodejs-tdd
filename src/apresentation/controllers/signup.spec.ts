902125a44a6d64b68f0f2ca0f0b8097dcfe5d828
Franklin Silva <franklinteixeira205@gmail.com>
2023-05-12 08:47:14 -0300

    feat: ensure SignUpController if conflict passwords

--- src/apresentation/controllers/signup.spec.ts
+++ src/apresentation/controllers/signup.spec.ts
@@ -2,6 +2,7 @@ import { describe, expect, test } from 'vitest';
 import { SignUpController } from './signup';
 import { HttpRequest } from '../protocols/http';
 import { MissingParamError } from '../errors/missing-param-error';
+import { InvalidParamError } from '../errors/invalid-param-error';
 
 const makeSUT = () => {
     const sut = new SignUpController();
@@ -69,6 +70,21 @@ describe('controller:signup', () => {
         const httpResponse = sut.handle(httpRequest);
         expect(httpResponse.statusCode).toBe(400);
         expect(httpResponse.body).toEqual(new MissingParamError('confirmPassword'));
+    });
+
+    test('should returns 400 if confirm password not equal password', () => {
+        const { sut } = makeSUT();
+        const httpRequest: HttpRequest = {
+            body: {
+                name: 'John Doe',
+                email: 'johndoe@gmail.com',
+                password: 'qwe123',
+                confirmPassword: '--invalid--'
+            }
+        };
+        const httpResponse = sut.handle(httpRequest);
+        expect(httpResponse.statusCode).toBe(400);
+        expect(httpResponse.body).toEqual(new InvalidParamError('confirmPassword'));
     });
 
 });
--- src/apresentation/controllers/signup.ts
+++ src/apresentation/controllers/signup.ts
@@ -1,3 +1,4 @@
+import { InvalidParamError } from '../errors/invalid-param-error';
 import { MissingParamError } from '../errors/missing-param-error';
 import { Controller } from '../protocols/controller';
 import { HttpRequest, HttpResponse } from '../protocols/http';
@@ -15,6 +16,13 @@ export class SignUpController implements Controller {
                     body: new MissingParamError(field)
                 };
             }
+        }
+
+        if (httpRequest.body.password != httpRequest.body.confirmPassword) {
+            return {
+                statusCode: 400,
+                body: new InvalidParamError('confirmPassword')
+            };
         }
 
         return {
