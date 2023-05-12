c44a916ae6d454c836fb7ccc8b2ba30cab383994
Franklin Silva <franklinteixeira205@gmail.com>
2023-05-12 09:20:40 -0300

    refactor: organize imports errors

--- src/apresentation/controllers/signup.spec.ts
+++ src/apresentation/controllers/signup.spec.ts
@@ -1,8 +1,7 @@
 import { describe, expect, test, vitest } from 'vitest';
 import { SignUpController } from './signup';
 import { HttpRequest } from '../protocols/http';
-import { MissingParamError } from '../errors/missing-param-error';
-import { InvalidParamError } from '../errors/invalid-param-error';
+import { MissingParamError, InvalidParamError } from '../errors';
 import { EmailValidator } from '../protocols/email-validator';
 
 const makeEmailValidator = () => {
