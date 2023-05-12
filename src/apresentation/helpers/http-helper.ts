fe4be99d9afaee890857354eaaf8ac03cb2574c9
Franklin Silva <franklinteixeira205@gmail.com>
2023-05-12 09:39:44 -0300

    feat: created http helper methods

---
+++ src/apresentation/helpers/http-helper.ts
@@ -0,0 +1,16 @@
+import { HttpResponse } from '../protocols/http';
+
+export const badRequest = (error: Error): HttpResponse => {
+    return {
+        statusCode: 400,
+        body: error
+    };
+};
+
+export const serverError = (error: Error): HttpResponse => {
+    return {
+        statusCode: 500,
+        body: error
+    };
+};
+
