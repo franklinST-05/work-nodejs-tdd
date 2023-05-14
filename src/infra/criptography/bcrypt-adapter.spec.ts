972d151d56135e110eebe30fe1265ad4b4b552db
Franklin Silva <franklinteixeira205@gmail.com>
2023-05-14 15:35:23 -0300

    test: ensure BcryptAdapter return a hash on success

--- src/infra/criptography/bcrypt-adapter.spec.ts
+++ src/infra/criptography/bcrypt-adapter.spec.ts
@@ -11,10 +11,21 @@ describe('criptography', () => {
     test('should call bcrypt with correct values', async () => {
         const salt = 12;
         const { sut } = makeSut(salt);
-        const hashSpy = vi.spyOn(bcrypt, 'hashSync');
+        const hashSpy = vi.spyOn(bcrypt, 'hash');
         const password = 'qwe123';
 
         await sut.encrypt(password);
         expect(hashSpy).toHaveBeenCalledWith(password, salt);
+    });
+
+    test('should return a hash on sucess', async () => {
+        const salt = 12;
+        vi.spyOn(bcrypt, 'hash').mockImplementationOnce(() => '--HASHED-PASSWORD--');
+        
+        const { sut } = makeSut(salt);
+        const password = 'qwe123';
+
+        const hash = await sut.encrypt(password);
+        expect(hash).toBe('--HASHED-PASSWORD--');
     });
 });
