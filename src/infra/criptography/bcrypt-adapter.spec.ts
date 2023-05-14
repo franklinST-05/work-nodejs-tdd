538ccaba0ad5b8f9069f3995c42731c67eb6f12e
Franklin Silva <franklinteixeira205@gmail.com>
2023-05-14 15:40:33 -0300

    refactor: add unique salt for all tests

--- src/infra/criptography/bcrypt-adapter.spec.ts
+++ src/infra/criptography/bcrypt-adapter.spec.ts
@@ -2,27 +2,27 @@ import bcrypt from 'bcrypt';
 import { describe, expect, test, vi } from 'vitest';
 import { BcryptAdapter } from './bcrypt-adapter';
 
-const makeSut = (salt:number): { sut: BcryptAdapter } => {
-    const sut = new BcryptAdapter(salt);
+const bcryptSalt = 12;
+
+const makeSut = (): { sut: BcryptAdapter } => {
+    const sut = new BcryptAdapter(bcryptSalt);
     return { sut };
 };
 
 describe('criptography', () => {
     test('should call bcrypt with correct values', async () => {
-        const salt = 12;
-        const { sut } = makeSut(salt);
+        const { sut } = makeSut();
         const hashSpy = vi.spyOn(bcrypt, 'hash');
         const password = 'qwe123';
 
         await sut.encrypt(password);
-        expect(hashSpy).toHaveBeenCalledWith(password, salt);
+        expect(hashSpy).toHaveBeenCalledWith(password, bcryptSalt);
     });
 
     test('should return a hash on sucess', async () => {
-        const salt = 12;
         vi.spyOn(bcrypt, 'hash').mockImplementationOnce(() => '--HASHED-PASSWORD--');
-        
-        const { sut } = makeSut(salt);
+
+        const { sut } = makeSut();
         const password = 'qwe123';
 
         const hash = await sut.encrypt(password);
