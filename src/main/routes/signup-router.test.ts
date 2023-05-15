168581ea0670574cf95c27524126de3063d6a607
Franklin Silva <franklinteixeira205@gmail.com>
2023-05-15 15:03:02 -0300

    refactor: connect to MongoDb before test

--- src/main/routes/signup-router.test.ts
+++ src/main/routes/signup-router.test.ts
@@ -1,6 +1,18 @@
 import request from 'supertest';
-import { describe, test } from 'vitest';
+import { afterAll, beforeAll, describe, test } from 'vitest';
+import { setup, teardown } from 'vitest-mongodb';
+import { MongoHelper } from '../../infra/database/mongodb/helpers/mongo-helper';
 import app from '../config/app';
+
+beforeAll(async () => {
+    await setup();
+    await MongoHelper.connect();
+});
+
+afterAll(async () => {
+    await MongoHelper.disconnect();
+    await teardown();
+});
 
 describe('route:signup', () => {
     test('should return an account on success', async () => {
