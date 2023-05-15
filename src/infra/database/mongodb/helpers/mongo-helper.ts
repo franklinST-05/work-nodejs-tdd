04f9cab2b10dc96ecc01657dd5102eb2f6f1e7af
Franklin Silva <franklinteixeira205@gmail.com>
2023-05-14 22:37:40 -0300

    feat: add mongo helper method get a new collection

--- src/infra/database/mongodb/helpers/mongo-helper.ts
+++ src/infra/database/mongodb/helpers/mongo-helper.ts
@@ -1,13 +1,17 @@
-import { MongoClient } from 'mongodb';
+import { Collection, MongoClient } from 'mongodb';
 
 export const MongoHelper = {
     client: null as unknown as MongoClient,
-    
+
     async connect(uri: string) {
         this.client = await MongoClient.connect(uri);
     },
 
     async disconnect() {
         this.client.close();
+    },
+
+    getCollection(name: string): Collection {
+        return this.client.db().collection(name);
     }
 };
